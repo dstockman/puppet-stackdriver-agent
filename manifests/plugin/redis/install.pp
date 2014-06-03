51029e7951a3a66ffc39a3239f83b82093565eb2
Pavel Shpilev <p.shpilev@gmail.com>
2014-06-03 16:56:56 +1000

    Fixing Redis plugin

--- manifests/plugin/redis/install.pp
+++ manifests/plugin/redis/install.pp
@@ -10,6 +10,6 @@ class stackdriver::plugin::redis::install inherits stackdriver::plugin::redis {
     fail("No package defined for OS ${::operatingsystem}")
   }
 
-  ensure_packages($pkg)
+  ensure_packages([$pkg])
 
 }
